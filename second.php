d28b3b44c014fdc6929f63e6f8faff3058eb7a35
Shantanu Singh Jhala <ssjhala91@gmail.com>
2019-10-07 23:45:24 +0530

    Update second.php

--- second.php
+++ second.php
@@ -1,15 +1,51 @@
 <?php
-if(isset($_SESSION['user']))
+if(isset($_SESSION['useridd']))
 {
 echo "already logged in";
 header("location:dashboard.php");
 }
 else
 {
-  header("location:conn.php");
-  session_start();
-  $_SESSION['user']=$userid;
+$conn = mysqli_connect("localhost", "root", "","dac");
 
-  header("location:dashboard.php");
+if (!$conn) 
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql = "SELECT username FROM users where username='".$_POST['userid']."' and password ='".$_POST['pass']."'"; 
+$result = mysqli_query($conn, $sql);
+
+$useridd=$_POST['userid'];
+echo "hiiii1";
+
+if (mysqli_num_rows($result) == 1) 
+   {
+
+     echo "hiiii2";
+       //  while($row = mysqli_fetch_assoc($result))
+      // {
+     //  echo "id: ".$row["username"]."<br>";
+    //  if($row["username"]==$useridd)
+   //     {
+    
+        session_start();
+        $_SESSION['useridd']=$useridd;
+        header("location:dashboard.php");
+  
+  
+        //     }
+       // }
+     
+ }
+else    {
+            echo "no result found";
+    
+     //    header("location:first.php");
+        }
+
+
+//mysqli_close($conn);
 }
 ?>
